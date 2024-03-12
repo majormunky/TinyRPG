b86ff6fe3cb62b3b7aa7cf84dd91e94daec7c099
Josh Bright <josh.bright@gmail.com>
2024-03-12 10:42:32 -0700

    Updated character panel to show job, added 3 more characters

--- src/resources/PlayerData.gd
+++ src/resources/PlayerData.gd
@@ -3,17 +3,77 @@ extends Node
 var characters = []
 
 
-func _ready():
+func create_character(data):
 	var player_data = Character.new()
-	player_data.character_name = "Zeno"
-	player_data.job_name = "Warrior"
-	player_data.level = 1
-	player_data.max_hit_points = 100
-	player_data.current_hit_points = 100
-	player_data.max_magic_points = 10
-	player_data.current_magic_points = 10
-	player_data.strength = 40
-	player_data.dexterity = 15
-	player_data.intelligence = 10
+	player_data.character_name = data["name"]
+	player_data.job_name = data["job_name"]
+	player_data.level = data["level"]
+	player_data.max_hit_points = data["max_hit_points"]
+	player_data.current_hit_points = data["current_hit_points"]
+	player_data.max_magic_points = data["max_magic_points"]
+	player_data.current_magic_points = data["current_magic_points"]
+	player_data.strength = data["strength"]
+	player_data.dexterity = data["dexterity"]
+	player_data.intelligence = data["intelligence"]
 	
-	characters.append(player_data)
+	return player_data
+
+
+func _ready():
+	var p1 = {
+		"name": "Zeno",
+		"job_name": "Warrior",
+		"level": 1,
+		"max_hit_points": 100,
+		"current_hit_points": 100,
+		"max_magic_points": 10,
+		"current_magic_points": 10,
+		"strength": 40,
+		"dexterity": 15,
+		"intelligence": 10
+	}
+	
+	var p2 = {
+		"name": "Torin",
+		"job_name": "Cleric",
+		"level": 1,
+		"max_hit_points": 60,
+		"current_hit_points": 60,
+		"max_magic_points": 40,
+		"current_magic_points": 40,
+		"strength": 20,
+		"dexterity": 15,
+		"intelligence": 40
+	}
+	
+	var p3 = {
+		"name": "SneakyCab",
+		"job_name": "Thief",
+		"level": 1,
+		"max_hit_points": 60,
+		"current_hit_points": 60,
+		"max_magic_points": 10,
+		"current_magic_points": 10,
+		"strength": 20,
+		"dexterity": 35,
+		"intelligence": 10
+	}
+	
+	var p4 = {
+		"name": "FireCab",
+		"job_name": "Mage",
+		"level": 1,
+		"max_hit_points": 40,
+		"current_hit_points": 40,
+		"max_magic_points": 50,
+		"current_magic_points": 50,
+		"strength": 15,
+		"dexterity": 15,
+		"intelligence": 40
+	}
+	
+	characters.append(create_character(p1))
+	characters.append(create_character(p2))
+	characters.append(create_character(p3))
+	characters.append(create_character(p4))
+	
--- src/scenes/Menu/CharacterPanel.gd
+++ src/scenes/Menu/CharacterPanel.gd
@@ -2,10 +2,14 @@ extends HBoxContainer
 
 var character_data: Character
 var portraits = {
-	"Warrior": preload("res://assets/characters/warrior.png")
+	"Warrior": preload("res://assets/characters/warrior.png"),
+	"Cleric": preload("res://assets/characters/cleric.png"),
+	"Thief": preload("res://assets/characters/thief.png"),
+	"Mage": preload("res://assets/characters/mage.png")
 }
 
-@onready var character_name = $MarginContainer/VBoxContainer/Name
+@onready var character_name = $MarginContainer/VBoxContainer/HBoxContainer/Name
+@onready var job = $MarginContainer/VBoxContainer/HBoxContainer/Job
 @onready var character_level = $MarginContainer/VBoxContainer/Level
 @onready var character_hp = $MarginContainer/VBoxContainer/HP
 @onready var character_mp = $MarginContainer/VBoxContainer/MP
@@ -22,10 +26,11 @@ func set_character_data(char_data: Character):
 
 func render_panel():
 	character_name.text = character_data.character_name
+	job.text = character_data.job_name
 	character_level.text = "Level: " + str(character_data.level)
 	character_hp.text = "HP: " + str(character_data.current_hit_points) + "/" + str(character_data.max_hit_points)
 	character_mp.text = "MP: " + str(character_data.current_magic_points) + "/" + str(character_data.max_magic_points)
-	portrait.texture = portraits["Warrior"]
+	portrait.texture = portraits[character_data.job_name]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
