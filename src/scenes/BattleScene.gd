8d1191ac632eb940e191004c488b710af360320d
Josh Bright <josh.bright@gmail.com>
2024-03-20 08:36:09 -0700

    Added a BattleScene

---
+++ src/scenes/BattleScene.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var monster_container = $CanvasLayer/HBoxContainer/MonsterListContainer
+@onready var player_container = $CanvasLayer/HBoxContainer/PlayerListContainer
+
+func update_player_container():
+	pass
